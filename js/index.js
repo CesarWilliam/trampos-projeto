ed6ce6fe6128c783d3f8b2493e270f52cf952f42
William Cesar <will.roc@hotmail.com>
2018-08-13 00:54:18 -0300

    fim versão desk

--- js/index.js
+++ js/index.js
@@ -6,3 +6,26 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    autoplay:true,
+    	autoplayTimeout:2000,
+	    margin:10,
+	    nav:false,
+	    dots: true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        768:{
+	            items:3
+	        },
+	        1200:{
+	            items:5
+	        }
+	    }
+	});
+});
