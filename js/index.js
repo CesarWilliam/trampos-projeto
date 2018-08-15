370cbbd2874b9e74e76058618b5c68969af6c50a
William Cesar <will.roc@hotmail.com>
2018-08-15 01:30:58 -0300

    ajustes mobile

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
 	    autoplay:true,
-    	autoplayTimeout:2000,
+    	autoplayTimeout:20000,
 	    margin:10,
 	    nav:false,
 	    dots: true,
